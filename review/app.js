2c0936060dcf4a41d0317caf34c2c117f1c4abc6
Jace Gold <jacegold@gmail.com>
2019-01-11 20:12:07 -0700

    Add purchase button and animations

--- review/app.js
+++ review/app.js
@@ -36,11 +36,26 @@ $(document).ready( function() {
       </li>
     */
     updateTotal();
+
+    // If cart is hidden, show it
+    var button = $('#display_cart');
+    if ( $('#display_cart').text() === 'Display Cart') {
+      button.text('Hide Cart');
+      $('#cart').slideToggle();
+    }
   }); // end add to cart
 
 
   function updateTotal() {
     var total = 0.00;
+
+    // Allow the user to clear cart
+    // We don't want the button to display if cart is empty
+    var entries = $('.entry');
+    if (entries.length)
+      $('#empty').show();
+    else
+      $('#empty').hide();
 
     $('.entry').each( function(index, entry) {
       var data = $(entry).data();
@@ -61,4 +76,46 @@ $(document).ready( function() {
     }); // end $('.entry').each()
     $("#total").text('$' + total);
   } // end updateTotal();
+
+
+  $('#empty').on('click', function () {
+    $('#in_cart').empty();
+    updateTotal();
+  }); // end empty cart
+
+
+  $('#in_cart').on('click', 'button.remove', function () {
+    $(this).parents('li').remove();
+    updateTotal();
+  }); // end remove item
+
+  $('#display_cart').on('click', function() {
+    var cart = $('#cart');
+    var button = $(this); // this being what user just clicked to get here
+    if (button.text() === 'Hide Cart')
+      button.text('Show Cart');
+    else
+      button.text('Hide Cart');
+
+    cart.slideToggle();    
+  }); // end display cart button onclick function
+
+  $('#purchase').on('click', function () {
+    $('#complete')
+      .html('<h2>PURCHASE COMPLETE<h2>')
+      .css({
+        'background-color': '#bca',
+        'width': '25%',
+        'border': '1px solid green',
+        'text-align': 'center'
+      })
+      .animate({
+        width: '70%',
+        opacity: 0.4,
+        marginLeft: '100px',
+        fontSize: '3em',
+        borderWidth: '10px',
+      }, 1500);
+  });
+
 });
